f862efa24fed69806469817cd3c5192414c2a9f0
alexandre <alexandre.awadallak@gmail.com>
2021-10-28 22:25:23 -0300

    feat: message domain

--- domain/message.go
+++ domain/message.go
@@ -1,4 +1,17 @@
 package domain
 
 type Message struct {
+	from    string
+	to      string
+	subject string
+	body    string
+}
+
+func NewMessage(from, to, subject, body string) Message {
+	return Message{
+		from:    from,
+		to:      to,
+		subject: subject,
+		body:    body,
+	}
 }
