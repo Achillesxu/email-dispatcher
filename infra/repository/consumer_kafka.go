48c383c2c9a569168c72cbc687ff13391bf058bf
alexandre <alexandre.awadallak@gmail.com>
2021-10-29 00:06:09 -0300

    updated: consumer repository

--- infra/repository/consumer_kafka.go
+++ infra/repository/consumer_kafka.go
@@ -3,9 +3,11 @@ package repository
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"latest/config"
 	domain "latest/domain/message"
 	"latest/dto"
+	"strings"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -17,8 +19,8 @@ type Consumer struct {
 func NewConsumer() *Consumer {
 	return &Consumer{
 		reader: kafka.NewReader(kafka.ReaderConfig{
-			Brokers: []string{config.GetConfig().Brokers},
-			GroupID: config.GetConfig().Topic,
+			Brokers: strings.Split(config.GetConfig().Brokers, ","),
+			GroupID: fmt.Sprintf("consumer-%s", config.GetConfig().Topic),
 			Topic:   config.GetConfig().Topic}),
 	}
 }
